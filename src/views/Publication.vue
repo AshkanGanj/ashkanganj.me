a20a9757e297fc04560a520a7a2cc9c5263b501d
Ashkan Ganj <ashkanganj@gmail.com>
2024-05-19 19:34:20 -0400

    updated coloring scheme

--- src/views/Publication.vue
+++ src/views/Publication.vue
@@ -2,10 +2,11 @@
   <div class="container border-bottom">
     <div class="text-start pt-4">
       <h2>Research</h2>
-      <p class="text-justify">My research is mainly focused on computer vision, 3D vision, and mobile Augmented Reality(AR). Currently, I am
+      <p class="text-justify">
+        My research is mainly focused on computer vision, 3D vision, and mobile Augmented Reality(AR). Currently, I am
         working on a fundamental task of 3D vision: depth estimation. I am looking to improve the accuracy and
-        robustness of current depth estimation models and better integrate these models into mobile AR scenarios.</p>
-
+        robustness of current depth estimation models and better integrate these models into mobile AR scenarios.
+      </p>
     </div>
     <div class="row">
       <div class="col-md-12" v-for="(publication, index) in publications" :key="index">
@@ -14,7 +15,8 @@
             <img class="img-fluid" :src="publication.img" alt="Publication Image" />
           </div>
           <div class="col-lg-9">
-            <div class="publication-item p-3 bg-body-tertiary">
+            <div
+              :class="['publication-item p-3', index % 2 === 0 ? 'bg-body-tertiary' : 'bg-body-secondary']">
               <div class="d-flex justify-content-between">
                 <h5 class="text-truncate" style="max-width: 85%;">{{ publication.title }}</h5>
                 <span class="d-none d-md-block">{{ publication.date }}</span>
@@ -32,7 +34,6 @@
                     </template>
                   </span>
                 </template>
-
               </div>
               <p>
                 <span class="" v-if="publication.venue">{{ publication.venue }} :</span>
@@ -42,23 +43,19 @@
                 🏆 {{ publication.award }}
               </div>
               <div class="btn-group publication-link">
-                <a v-if="publication.link" :href="publication.link" target="_blank" class="btn btn-outline-secondary"><i
-                    class="bi bi-file-earmark-pdf"></i> Paper</a>
-                <a v-if="publication.website" :href="publication.website" target="_blank"
-                  class="btn btn-outline-secondary"><i class="bi bi-globe"></i> Website</a>
-
-                <a v-if="publication.github" :href="publication.github" target="_blank"
-                  class="btn btn-outline-secondary"><i class="bi bi-github"></i> Code</a>
-                <a v-if="publication.arxiv" :href="publication.arxiv" target="_blank"
-                  class="btn btn-outline-secondary">arxiv</a>
+                <a v-if="publication.link" :href="publication.link" target="_blank" class="btn btn-outline-secondary">
+                  <i class="bi bi-file-earmark-pdf"></i> Paper
+                </a>
+                <a v-if="publication.website" :href="publication.website" target="_blank" class="btn btn-outline-secondary">
+                  <i class="bi bi-globe"></i> Website
+                </a>
+                <a v-if="publication.github" :href="publication.github" target="_blank" class="btn btn-outline-secondary">
+                  <i class="bi bi-github"></i> Code
+                </a>
+                <a v-if="publication.arxiv" :href="publication.arxiv" target="_blank" class="btn btn-outline-secondary">
+                  arxiv
+                </a>
               </div>
-              <!-- <div>
-                <a v-if="publication.link" :href="publication.link" target="_blank" class="badge-btn">Paper</a>
-                <a v-if="publication.website" :href="publication.website" target="_blank" class="badge-btn">Project</a>
-                <a v-if="publication.arxiv" :href="publication.arxiv" target="_blank" class="badge-btn">arxiv</a>
-                <a v-if="publication.github" :href="publication.github" target="_blank" class="badge-btn">Code</a>
-              </div> -->
-
             </div>
           </div>
         </div>
@@ -66,6 +63,7 @@
     </div>
   </div>
 </template>
+
 <script>
 export default {
   data() {
